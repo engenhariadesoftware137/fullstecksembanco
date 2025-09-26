2285c135ad6720338a054f28fc02dba5ee1ea4c2
Rodrigo Gomes <engenhariadesoftware137@gmail.com>
2025-09-26 07:03:47 -0300

    primeiro

---
+++ SistemaAluno/Repositorio/IEstudanteRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public interface IEstudanteRepositorio
+    {
+        void Salvar(Estudante estudante);
+        List<Estudante> ObterTodos();
+        Estudante ObterPorId(int id);
+        void Atualizar(Estudante estudante);
+        void Remover(int id);
+    }
+}
---
+++ SistemaAluno/Servico/IEstudanteServico.cs
@@ -0,0 +1,18 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servico
+{
+    public interface IEstudanteServico
+    {
+        void AdicionarEstudante(string nome, decimal nota);
+        List<Estudante> ObterTodos();
+        Estudante ObterPorId(int id);
+        void AtualizarEstudante(Estudante estudante);
+        void RemoverEstudante(int id);
+    }
+}
